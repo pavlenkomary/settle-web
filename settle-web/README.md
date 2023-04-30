ff4864ddece0ccbe376b68982eeff794cc846cc2
pavlenkomary <60288439+pavlenkomary@users.noreply.github.com>
2023-04-30 20:42:46 +0100

    Add files via upload

---
+++ settle-web/README.md
@@ -0,0 +1,18 @@
+TeleFi: Bringing seamless Web3 Payments straight to telegram.
+
+# Steps to connect to TeleFi
+
+As a payment requester, 
+
+1. Add the @tele_fi_bot on TG
+2. Connect your Wallet
+3. Share payment request to your Friend
+----
+
+As a payment sender,
+
+1. Get notified of a payment request.
+2. Tap on payment request and open @TeleFi Bot on TG
+3. Connect wallet, reviews terms and sign transaction
+
+# Learn more
---
+++ settle-web/src/main.tsx
@@ -0,0 +1,26 @@
+import { EthereumClient } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { WagmiConfig } from 'wagmi'
+
+import { App } from './App'
+import { chains, client, walletConnectProjectId } from './wagmi'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+const ethereumClient = new EthereumClient(client, chains)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <WagmiConfig client={client}>
+        <App/>
+        <Web3Modal
+          projectId={walletConnectProjectId}
+          ethereumClient={ethereumClient}
+        />
+      </WagmiConfig>
+    </ChakraProvider>
+  </React.StrictMode>,
+)
